b6b6ca816391be2320806f3da4f81fff23dfb511
mloran <94021746+mloran@users.noreply.github.com>
2023-07-26 11:24:15 +0200

    added some animations

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+const slides = document.querySelector('#slides');
+const prevBtn = document.querySelector('#prev-btn')
+const nextBtn = document.querySelector('#next-btn')
+const pics = ["https://img.fcbayern.com/image/upload/v1633517110/cms/public/images/allianz-arena/europameisterschaft/UEFA-EURO2024-KV_UEFA.png","https://img.uefa.com/imgml/uefacom/euro2024/rebrand/og-image.png","https://www.fussballnationalmannschaft.net/wp-content/uploads/2021/10/uefa-euro-2024-logo-scaled.jpg","https://editorial.uefa.com/resources/0282-184c9ffe0c0f-9c65bdcec1ab-1000/slide_9_-_16x9.png"]
 
-// // Just a Test Code
+let currentSlideIndex = 0;
 
-// let cats = ["blue", "kity"];
+//Sliding for the left button 
+prevBtn.addEventListener('click', function () {
+    slides.classList.add('fade-out'); 
 
-// let dogs = ["rusty", "wyatt"];
+  setTimeout(function () {
+    currentSlideIndex = (currentSlideIndex - 1 + pics.length) % pics.length;
+    slides.src = pics[currentSlideIndex];
+    slides.classList.remove('fade-out'); 
+  }, 500); 
+});
 
-// let combo = cats.concat(dogs);
+//Sliding for the right button 
+nextBtn.addEventListener('click', function () {
+    slides.classList.add('fade-out'); 
 
-
-// let test_alert = prompt("Wie geht es dir heute so?")
+  setTimeout(function () {
+    currentSlideIndex = (currentSlideIndex + 1) % pics.length;
+    slides.src = pics[currentSlideIndex];
+    slides.classList.remove('fade-out'); 
+  }, 500); 
+});
